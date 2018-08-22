88c3425992db5859b5fcb0345d977728d22f0c6a
vulcan9 <pdi1066@naver.com>
2018-08-22 16:12:23 +0900

    .

---
+++ docs/libs/main.js
@@ -0,0 +1,13 @@
+var jj = (function(jj){
+	if(jj) return;
+	jj = {
+		link: {
+			//jj.link.html('./index.html', '_blank', {parameter: 'ver/release_0.4.md'})
+			html: function(index, target, obj){
+				var page = '/jik-ji-Binder/' + obj.parameter:
+				window.open(page, (target || '_self'));
+			}
+		}
+	};
+	return jj;
+})(window.jj);
